6f5e495ffcf66e52cfd0f6a91e7a44461edb92a1
Yoga Rizki Pratama <110215272+yogaxdd@users.noreply.github.com>
2025-05-09 20:27:31 +0700

    Setup Laravel for Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,12 @@
 <?php
 
-require __DIR__ . '/../public/index.php';   
+// sesuaikan path ke bootstrap/app.php
+require __DIR__ . '/../bootstrap/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// jalankan kernel
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = tap($kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+))->send();
+$kernel->terminate($request, $response);
